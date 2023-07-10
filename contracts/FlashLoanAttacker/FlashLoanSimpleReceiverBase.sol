d76528ff6628bd579f10701e23428911390aaf7f
Trang Duc Nguyen <lehongvi19x@gmail.com>
2023-07-10 22:21:06 +0700

    update smart contract loanFlashAttacker

---
+++ contracts/FlashLoanAttacker/FlashLoanSimpleReceiverBase.sol
@@ -0,0 +1,36 @@
+/*
+  
+ ██████  ██████   ██████  ██   ██ ██████   ██████   ██████  ██   ██    ██████  ███████ ██    ██
+██      ██    ██ ██    ██ ██  ██  ██   ██ ██    ██ ██    ██ ██  ██     ██   ██ ██      ██    ██
+██      ██    ██ ██    ██ █████   ██████  ██    ██ ██    ██ █████      ██   ██ █████   ██    ██
+██      ██    ██ ██    ██ ██  ██  ██   ██ ██    ██ ██    ██ ██  ██     ██   ██ ██       ██  ██
+ ██████  ██████   ██████  ██   ██ ██████   ██████   ██████  ██   ██ ██ ██████  ███████   ████
+
+Find any smart contract, and build your project faster: https://www.cookbook.dev
+Twitter: https://twitter.com/cookbook_dev
+Discord: https://discord.gg/WzsfPcfHrk
+
+Find this contract on Cookbook: https://www.cookbook.dev/contracts/Flash-loan-attacker/?utm=code
+*/
+
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity 0.8.10;
+
+import {IFlashLoanSimpleReceiver} from './IFlashLoanSimpleReceiver.sol';
+import {IPoolAddressesProvider} from './IPoolAddressesProvider.sol';
+import {IPool} from './IPool.sol';
+
+/**
+ * @title FlashLoanSimpleReceiverBase
+ * @author Aave
+ * @notice Base contract to develop a flashloan-receiver contract.
+ */
+abstract contract FlashLoanSimpleReceiverBase is IFlashLoanSimpleReceiver {
+  IPoolAddressesProvider public immutable override ADDRESSES_PROVIDER;
+  IPool public immutable override POOL;
+
+  constructor(IPoolAddressesProvider provider) {
+    ADDRESSES_PROVIDER = provider;
+    POOL = IPool(provider.getPool());
+  }
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,26 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.18",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        }
+      },
+      {
+        version: "0.8.10",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        }
+      },
+    ],
+  },
 };
